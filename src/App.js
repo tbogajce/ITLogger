48543bfd0a1b87c41c2e312d7a687c2f4b8afd65
Tihomir Bogajcevic <tihomirbogajcevic94@hotmail.com>
2020-06-04 16:45:40 +0200

    Created all components and modals needed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
+
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
@@ -9,7 +17,19 @@ const App = () => {
         M.AutoInit();
     });
 
-    return <div className='App'>My App</div>;
+    return (
+        <Fragment>
+            <SearchBar />
+            <div className='container'>
+                <AddBtn />
+                <AddLogModal />
+                <EditLogModal />
+                <AddTechModal />
+                <TechListModal />
+                <Logs />
+            </div>
+        </Fragment>
+    );
 };
 
 export default App;
